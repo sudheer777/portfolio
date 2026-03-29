4a5d667ec7dacb3bd6b855fe4440db8cdba40c23
sudheer777 <sudheerpendyala7@gmail.com>
2026-03-29 20:38:27 +0530

    minor bug

--- go_app/frontend/src/main.tsx
+++ go_app/frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
 
-registerSW({ immediate: true })
+registerSW()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
